d3ecdcddbb96a7be997a1cf6d6596e45bf41237c
dmacias-beep <dmacias@iespompeufabra.cat>
2026-09-23 14:16:16 +0200

    Create sketch.js

---
+++ cara1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(42,133,3);
+  ellipse(300,300,200,250)
+  ellipse(250,250,50,30)
+  ellipse(350,250,50,30)
+  arc(300,350,120,40,0,PI);
+  triangle(300,300,330,360,310,320)
+}
